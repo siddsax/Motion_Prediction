8922f7eebaa3426c83c18ffb1e65ca882df7270c
siddsax <ss.siddharthasaxena@gmail.com>
2018-04-13 06:09:10 +0530

    homo +

--- GCN_new/neuralmodels/layers/GraphConvolution.py
+++ GCN_new/neuralmodels/layers/GraphConvolution.py
@@ -133,33 +133,41 @@ class GraphConvolution_homo(object):
 		if (self.weights):
 			for param, weight in zip(self.params, self.weights):
 				param.set_value(np.asarray(weight, dtype=theano.config.floatX))
-
+		self.nonzeros = {}
+                for i in range(np.shape(self.adjacency)[0]):
+                        count = 0
+                        self.nonzeros[i] = []
+                        for j in range(np.shape(self.adjacency)[1]):
+                                if(self.adjacency[i, j]):
+                                        self.nonzeros[i].append(j)
+                                        count += 1
+		
 		# for i in range(len(self.W)):
 		# 	self.L2_sqr = (self.W[i] ** 2).sum()
 		self.L2_sqr = (self.W ** 2).sum()
 
 	def output(self, seq_output=True):
 		x = self.layer_below.output(seq_output=seq_output)
-		pre_sup = T.tensordot(x, self.W, axes=[3, 0])
-		support = T.tensordot(self.adjacency, pre_sup, axes=[0, 2])
-		sp = support.shape
-		support = support.reshape((sp[1], sp[2], sp[0], sp[3]))
-		output = support
+		#pre_sup = T.tensordot(x, self.W, axes=[3, 0])
+		#support = T.tensordot(self.adjacency, pre_sup, axes=[0, 2])
+		#sp = support.shape
+		#support = support.reshape((sp[1], sp[2], sp[0], sp[3]))
+		#output = support
 
-		if self.bias:
-			output += self.b
+		#if self.bias:
+		#	output += self.b
 
-		# for i in range(np.shape(self.adjacency)[0]):
-		# 	for j in range(1, len(self.nonzeros[i])):
-		# 		if(j==0):
-		# 			out_d = T.tensordot(x[:, :, self.nonzeros[i][j], :],self.W[i][j, :, :], axes=[2, 0])
-		# 		else:
-		# 			out_d += T.tensordot(x[:, :, self.nonzeros[i][j], :],self.W[i][j, :, :], axes=[2, 0])
-		# 		if self.bias:
-		# 			out_d += self.b[i][j, :]
-		# 	if(i == 0):
-		# 		out = out_d.reshape((out_d.shape[0], out_d.shape[1], 1, out_d.shape[2]))
-		# 	else:
-		# 		out = T.concatenate((out, out_d.reshape((out_d.shape[0], out_d.shape[1], 1, out_d.shape[2]))), axis=2)
+		for i in range(np.shape(self.adjacency)[0]):
+		 	for j in range(1, len(self.nonzeros[i])):
+		 		if(j==1):
+		 			out_d = T.tensordot(x[:, :, self.nonzeros[i][j], :],self.W, axes=[2, 0])
+		 		else:
+		 			out_d += T.tensordot(x[:, :, self.nonzeros[i][j], :],self.W, axes=[2, 0])
+		 		if self.bias:
+		 			out_d += self.b[i][j, :]
+		 	if(i == 0):
+		 		out = out_d.reshape((out_d.shape[0], out_d.shape[1], 1, out_d.shape[2]))
+		 	else:
+		 		out = T.concatenate((out, out_d.reshape((out_d.shape[0], out_d.shape[1], 1, out_d.shape[2]))), axis=2)
 
 		return self.activation(out)
